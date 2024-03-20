465426657e7da1a2babfe152a21a449f6eb7fff5
vJEKKYv <wpdnjs20@gmail.com>
2024-03-20 17:23:51 +0900

    16페이지 마저 하기

--- app/src/main/java/com/example/chap06_1_activity/MainActivity.kt
+++ app/src/main/java/com/example/chap06_1_activity/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.chap06_1_activity
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -11,17 +13,28 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //enableEdgeToEdge()
+        setContentView(R.layout.visibility)
         /*
-        setContentView(R.layout.activity_main)
+
         setContentView(R.layout.activity_view)
         setContentView(R.layout.layout_select)
         setContentView(R.layout.set_padding)
-         */
         setContentView(R.layout.set_visible)
+         */
         /*
         val textView = findViewById<TextView>(R.id.text1)
         textView.text = "잘된다!!!!!"
          */
+        val visibleBtn = findViewById<Button>(R.id.visibleBtn)
+        val invisibleBtn = findViewById<Button>(R.id.invisibleBtn)
+        val targetView = findViewById<TextView>(R.id.targetView)
+        visibleBtn.setOnClickListener{
+            targetView.visibility = View.VISIBLE
+        }
+        val invisibleBtn = findViewById<Button>(R.id.invisibleBtn)
+        visibleBtn.setOnClickListener{
+            targetView.visibility = View.INVISIBLE
+        }
         /*
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
