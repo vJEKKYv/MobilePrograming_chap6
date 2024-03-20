ecb231a9e2a9c221301816d3b39939c07562bc02
vJEKKYv <wpdnjs20@gmail.com>
2024-03-20 16:29:11 +0900

    2중 LinearLayout

--- app/src/main/java/com/example/chap06_1_activity/MainActivity.kt
+++ app/src/main/java/com/example/chap06_1_activity/MainActivity.kt
@@ -9,8 +9,9 @@ import androidx.core.view.WindowInsetsCompat
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        //enableEdgeToEdge()
+        //setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_view)
         /*
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
