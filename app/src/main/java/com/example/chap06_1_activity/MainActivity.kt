3f76f3b57afc670fca1840b34b44980f36b775dd
vJEKKYv <wpdnjs20@gmail.com>
2024-03-20 16:44:22 +0900

    MainActivity.kt에서 text 넣기

--- app/src/main/java/com/example/chap06_1_activity/MainActivity.kt
+++ app/src/main/java/com/example/chap06_1_activity/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.chap06_1_activity
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -12,6 +13,9 @@ class MainActivity : AppCompatActivity() {
         //enableEdgeToEdge()
         setContentView(R.layout.activity_main)
         //setContentView(R.layout.activity_view)
+
+        val textView = findViewById<TextView>(R.id.text1)
+        textView.text = "잘된다!!!!!"
         /*
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
