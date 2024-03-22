e853a025ff6acd2ed6d0773f7e9b86a43d9b837c
vJEKKYv <wpdnjs20@gmail.com>
2024-03-22 14:47:19 +0900

    id pw 입력칸 만들기

--- app/src/main/java/com/example/chap06_1_activity/MainActivity.kt
+++ app/src/main/java/com/example/chap06_1_activity/MainActivity.kt
@@ -1,19 +1,14 @@
 package com.example.chap06_1_activity
 
 import android.os.Bundle
-import android.view.View
-import android.widget.Button
-import android.widget.TextView
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.example.chap06_1_activity.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        /*
         //Grandle/build.grandle.kts 중 app에 들어가 viewBinding {enable = true } 넣으면
         //activity_main 안에서 선언된 id를 binding.xx로 가져올 수 있다. val로 하나하나 선언 해 줄 필요 없다.
         val binding = ActivityMainBinding.inflate(layoutInflater)
@@ -24,6 +19,8 @@ class MainActivity : AppCompatActivity() {
         binding.visibleBtn.setOnClickListener {
             binding.targetView.visibility = View.VISIBLE
         }
+
+         */
         /*
         setContentView(R.layout.textlinetest)
         setContentView(R.layout.visibility)
