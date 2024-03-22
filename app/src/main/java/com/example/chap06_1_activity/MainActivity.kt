b0b79d196ded25a68b2d79ac6ea318658bafd88d
vJEKKYv <wpdnjs20@gmail.com>
2024-03-22 14:15:44 +0900

    3월 22일 수업 중간 커밋. dp, link, text축약

--- app/src/main/java/com/example/chap06_1_activity/MainActivity.kt
+++ app/src/main/java/com/example/chap06_1_activity/MainActivity.kt
@@ -13,9 +13,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //enableEdgeToEdge()
-        setContentView(R.layout.visibility)
+        setContentView(R.layout.checkbox_radiogroup_test)
         /*
-
+        setContentView(R.layout.textlinetest)
+        setContentView(R.layout.visibility)
         setContentView(R.layout.activity_view)
         setContentView(R.layout.layout_select)
         setContentView(R.layout.set_padding)
@@ -25,16 +26,21 @@ class MainActivity : AppCompatActivity() {
         val textView = findViewById<TextView>(R.id.text1)
         textView.text = "잘된다!!!!!"
          */
+        /*
         val visibleBtn = findViewById<Button>(R.id.visibleBtn)
-        val invisibleBtn = findViewById<Button>(R.id.invisibleBtn)
+        val invisibleBtn:Button = findViewById<Button>(R.id.invisibleBtn)
         val targetView = findViewById<TextView>(R.id.targetView)
         visibleBtn.setOnClickListener{
             targetView.visibility = View.VISIBLE
         }
-        val invisibleBtn = findViewById<Button>(R.id.invisibleBtn)
         visibleBtn.setOnClickListener{
+            targetView.visibility = View.VISIBLE
+        }
+        invisibleBtn.setOnClickListener{
             targetView.visibility = View.INVISIBLE
         }
+         */
+
         /*
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
